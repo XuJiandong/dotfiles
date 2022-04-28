4fa44d9f7b8eeed87db82c35e89658509e1448e1
xjd <lynndon@gmail.com>
2022-04-28 15:55:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,6 @@ chmod 644 ~/.ssh/id_rsa.pub
 echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-echo 'export PS1="$ "' >> ~/.bashrc
-echo 'export PS1="[%1d]$ "' >> ~/.zshrc
-
 git config --global user.name $GIT_USER
 git config --global user.email $GIT_EMAIL
 
